8002ef49528a56d0ad34498309543e45a28353c1
nvt <nvt@nvt.viettel.vn>
2026-07-20 22:28:15 +0700

    fix comment error

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,18 @@
 return {
   'numToStr/Comment.nvim',
   opts = {
-    -- add any options here
+    toggler = {
+      -- Toggle the current line with <leader>gc (Space g c).
+      -- Keep the plugin defaults such as `gc` (operator-pending) and `gcc`.
+      line = '<leader>gc',
+    },
+    pre_hook = function(ctx)
+      -- Avoid Comment.nvim's Tree-sitter comment detection for C-family buffers.
+      -- Its failure path reports the unhelpful "[Comment.nvim] nil" message.
+      if vim.bo.filetype == 'c' or vim.bo.filetype == 'cpp' then
+        return ctx.ctype == require('Comment.utils').ctype.blockwise and '/*%s*/' or '//%s'
+      end
+    end,
   },
   lazy = false,
   config = function(_, opts)
