5e9c4ad7bb4d84137e46552a84d2ed57397c0831
nvt <nvt@nvt.viettel.vn>
2026-09-02 21:39:57 +0700

    update something

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -3,9 +3,11 @@ return {
   config = function()
     require('auto-session').setup {
       log_level = 'error',
-      auto_session_enable_last_session = true,
+      auto_session_enable_last_session = false,
       auto_session_root_dir = vim.fn.stdpath 'data' .. '/sessions/',
-      auto_session_enabled = true,
+      auto_session_enabled = false,
+      auto_save_enabled = false,
+      auto_restore_enabled = false,
     }
   end,
 }
